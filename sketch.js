33f532f245f13d634ae9e7ed1e3abf75c528435f
Livteshwar <84121496+Livteshwar@users.noreply.github.com>
2021-06-17 19:37:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,22 +4,26 @@ var seaImg
 var shipImg
 function preload(){
   seaImg = loadImage("sea.png")
-  shipImg = loadAnimation("ship1.png,ship2.png,ship3.png,ship4.png")
+  shipImg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
 
 }
 
 function setup(){
-  createCanvas(600,200);
-  sea = createSprite(200,100,)
-  ship = createSprite(100,100,20,50)
+  createCanvas(400,400);
+  sea = createSprite(200,200)
+  ship = createSprite(100,200,20,50)
   sea.addImage(seaImg)
   ship.addAnimation("sailing",shipImg)
   sea.velocityX=-5
+  ship.scale=0.2
+  sea.scale=0.3
 }
 
 function draw() {
+
   background("blue");
  if (sea.x<0){
-   sea.x=sea.width/2
+   sea.x=sea.width/8
  }
+ drawSprites()
 }
